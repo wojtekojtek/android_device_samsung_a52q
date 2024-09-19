890a3de45f941b204b46b683c27a72ccb75269b6
wojtekojtek <49075395+wojtekojtek@users.noreply.github.com>
2024-09-19 07:25:17 +0200

    Update and rename lineage_a52q.mk to derp_a52q.mk

--- derp_a52q.mk
+++ derp_a52q.mk
@@ -24,7 +24,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
@@ -34,7 +34,7 @@ TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_a52q
+PRODUCT_NAME := derp_a52q
 PRODUCT_DEVICE := a52q
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A525F
@@ -48,14 +48,4 @@ BUILD_FINGERPRINT := "samsung/a52qnsxx/a52q:11/RP1A.200720.012/A525FXXS7FXE2:use
 PRIVATE_BUILD_DESC := "a52qnsxx-user 11 RP1A.200720.012 A525FXXS7FXE2 release-keys"
 
 # Security patch
-# VENDOR_SECURITY_PATCH := 2024-05-01
-
-# Everest
-EVEREST_MAINTAINER := "wojtekojtek"
-TARGET_SUPPORTS_BLUR := true
-TARGET_HAS_UDFPS := true
-EXTRA_UDFPS_ANIMATIONS := true
-WITH_GAPPS := true
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_DEFAULT_PIXEL_LAUNCHER := true
-TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
+VENDOR_SECURITY_PATCH := 2024-05-01

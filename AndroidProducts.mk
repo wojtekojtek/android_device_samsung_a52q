e6430b88e06695de39f1691e2fcc2bedbf9d3c69
wojtekojtek <{ID}+wojtekojtek@users.noreply.github.com>
2025-05-12 10:51:08 +0200

    a52q: Update lunch choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_a52q.mk
+    $(LOCAL_DIR)/infinity_a52q.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_a52q-userdebug \
-    lineage_a52q-user \
-    lineage_a52q-eng
+    infinity_a52q-userdebug \
+    infinity_a52q-user \
+    infinity_a52q-eng

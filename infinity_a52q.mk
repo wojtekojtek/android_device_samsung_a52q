bed8920c86cbc15d88ab25057e6ef53a1b61eb19
wojtekojtek <{ID}+wojtekojtek@users.noreply.github.com>
2025-05-12 10:52:52 +0200

    a52q: Fixed a typo

--- infinity_a52q.mk
+++ infinity_a52q.mk
@@ -28,7 +28,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 # Inherit some common Infinity stuff
 $(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
-# Enable utrueing of APEXes
+# Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Boot animation

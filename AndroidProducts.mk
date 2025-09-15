7d1d15d790fe4364a51e3ecd5f915b5a0643a49a
wojtekojtek <49075395+wojtekojtek@users.noreply.github.com>
2025-09-15 22:10:37 +0200

    halcyon-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_a52q.mk
+    $(LOCAL_DIR)/halcyon_a52q.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_a52q-userdebug \
-    lineage_a52q-user \
-    lineage_a52q-eng
+    halcyon_a52q-userdebug \
+    halcyon_a52q-user \
+    halcyon_a52q-eng
